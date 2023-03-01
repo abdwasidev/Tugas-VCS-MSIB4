b25b15688a9c06ff2a569a3fdb2c95817bbfe4be
Abdul Wasi' Al-Afif <73805930+abdwasidev@users.noreply.github.com>
2023-03-01 14:40:37 +0700

    Create call.js

---
+++ call.js
@@ -0,0 +1,12 @@
+//function that adds two numbers 
+function sum(a, b) {
+  return a + b;
+}
+
+// calling sum() function  
+var result = sum.call(this, 5, 10);
+
+console.log(result);
+
+//Output:
+// 15
